d98b9c878dd707ed9c36ee05ee21d639915cf6e7
zedrian <artsiom.kopats@gmail.com>
2016-04-18 15:44:51 +0300

    run_finder.py: construct_peak_from_file() now works correct.

--- run_finder.py
+++ run_finder.py
@@ -6,45 +6,45 @@ def parse_raw_line_to_components(raw_line):
     components = raw_line[:-1].replace('\t', ' ').split(' ')
     return [x for x in components if x != '']
 
-
-index = 0
-peaks = []
-with open(input('Enter the input file name: '), 'r+') as f:
-    while True:
-        line = f.readline()
-        if line == '':
-            break
-
-        elements = parse_raw_line_to_components(line)
-        if len(elements) == 0:
-            continue
-        if elements[0].upper() == 'ID':
-            peak = Peak()
-            peak.name = elements[1]
-            peak.index = index
-
-            elements_a = parse_raw_line_to_components(f.readline())
-            if elements_a[0].upper() == 'LOW':
-                for i in range(0, 10):
-                    elements_low = parse_raw_line_to_components(f.readline())
-                    peak.lowMass.append(float(elements_low[0]))
-                    peak.lowIntensity.append(float(elements_low[1]))
-
-            elements_a = parse_raw_line_to_components(f.readline())
-            if elements_a[0].upper() == 'MID':
-                for i in range(0, 10):
-                    elements_mid = parse_raw_line_to_components(f.readline())
-                    peak.midMass.append(float(elements_mid[0]))
-                    peak.midIntensity.append(float(elements_mid[1]))
-
-            elements_a = parse_raw_line_to_components(f.readline())
-            if elements_a[0].upper() == 'HIGH':
-                for i in range(0, 10):
-                    elements_high = parse_raw_line_to_components(f.readline())
-                    peak.highMass.append(float(elements_high[0]))
-                    peak.highIntensity.append(float(elements_high[1]))
-            index += 1
-            peaks.append(peak)
+#
+# index = 0
+# peaks = []
+# with open(input('Enter the input file name: '), 'r+') as f:
+#     while True:
+#         line = f.readline()
+#         if line == '':
+#             break
+#
+#         elements = parse_raw_line_to_components(line)
+#         if len(elements) == 0:
+#             continue
+#         if elements[0].upper() == 'ID':
+#             peak = Peak()
+#             peak.name = elements[1]
+#             peak.index = index
+#
+#             elements_a = parse_raw_line_to_components(f.readline())
+#             if elements_a[0].upper() == 'LOW':
+#                 for i in range(0, 10):
+#                     elements_low = parse_raw_line_to_components(f.readline())
+#                     peak.lowMass.append(float(elements_low[0]))
+#                     peak.lowIntensity.append(float(elements_low[1]))
+#
+#             elements_a = parse_raw_line_to_components(f.readline())
+#             if elements_a[0].upper() == 'MID':
+#                 for i in range(0, 10):
+#                     elements_mid = parse_raw_line_to_components(f.readline())
+#                     peak.midMass.append(float(elements_mid[0]))
+#                     peak.midIntensity.append(float(elements_mid[1]))
+#
+#             elements_a = parse_raw_line_to_components(f.readline())
+#             if elements_a[0].upper() == 'HIGH':
+#                 for i in range(0, 10):
+#                     elements_high = parse_raw_line_to_components(f.readline())
+#                     peak.highMass.append(float(elements_high[0]))
+#                     peak.highIntensity.append(float(elements_high[1]))
+#             index += 1
+#             peaks.append(peak)
 
 
 def construct_peak_from_file(file_name):
@@ -57,28 +57,34 @@ def construct_peak_from_file(file_name):
             if line == '':
                 break
 
-            if line[0].upper() == 'ENERGY0\n':
-                while parse_raw_line_to_components(file.readline())[0].upper() != 'ENERGY1\n':
-                    elements_low = parse_raw_line_to_components(file.readline())
+            if line.upper() == 'ENERGY0\n':
+                line = file.readline()
+                while line.upper() != 'ENERGY1\n':
+                    elements_low = parse_raw_line_to_components(line)
                     peak.lowMass.append(float(elements_low[0]))
                     peak.lowIntensity.append(float(elements_low[1]))
+                    line = file.readline()
 
-            if line[0].upper() == 'ENERGY1\n':
-                while parse_raw_line_to_components(file.readline())[0].upper() != 'ENERGY2\n':
-                    elements_mid = parse_raw_line_to_components(file.readline())
+            if line.upper() == 'ENERGY1\n':
+                line = file.readline()
+                while line.upper() != 'ENERGY2\n':
+                    elements_mid = parse_raw_line_to_components(line)
                     peak.midMass.append(float(elements_mid[0]))
                     peak.midIntensity.append(float(elements_mid[1]))
+                    line = file.readline()
 
-            if parse_raw_line_to_components(file.readline())[0].upper() == 'ENERGY2\n':
-                while parse_raw_line_to_components(file.readline())[0].upper() != '\n':
-                    elements_high = parse_raw_line_to_components(file.readline())
+            if line.upper() == 'ENERGY2\n':
+                line = file.readline()
+                while line.upper() != '\n':
+                    elements_high = parse_raw_line_to_components(line)
                     peak.highMass.append(float(elements_high[0]))
                     peak.highIntensity.append(float(elements_high[1]))
+                    line = file.readline()
 
         return peak
 
-# print('peaks loaded: {0}'.format(index))
-# print()
-# for peak in peaks:
-#     print(peak)
-#     print()
+
+if __name__ == '__main__':
+    peaks = []
+    peaks.append(construct_peak_from_file('database/B4154'))
+    print(peaks)
